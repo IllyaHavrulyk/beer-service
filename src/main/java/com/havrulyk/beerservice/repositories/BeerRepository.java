eb6773771128b75609dbfc14e3ede5c529a1c45e
IllyaHavrulyk <illyahavrulyk@gmail.com>
2021-12-27 23:10:49 +0200

    Added beer repository.

---
+++ src/main/java/com/havrulyk/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.havrulyk.beerservice.repositories;
+
+import com.havrulyk.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
